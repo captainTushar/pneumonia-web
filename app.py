91a1321b3e21804bfbd8ce14b388d9ebcbf37f10
captainTushar <tusharkandpal792@gmail.com>
2025-11-02 00:42:51 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def predict():
     return jsonify({"prediction": label})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
